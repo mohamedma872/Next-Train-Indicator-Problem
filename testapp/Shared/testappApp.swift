09df481adbf6295a07d70002b71d64e3cd550890
mohammed Elsdody <31186483+mohamedma872@users.noreply.github.com>
2022-08-19 16:43:03 +0200

    no message

---
+++ testapp/Shared/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  Shared
+//
+//  Created by Mohamed Elsdody on 18/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
